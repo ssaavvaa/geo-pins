85a4bf8c6c668ebdb5428d0f42c911b95ecea9a0
ssaavvaa <Toxasava@icloud.com>
2019-08-27 17:48:46 -0400

    Updates

---
+++ precache-manifest.e3ff59afd69e3c749cf3de334bfbcf25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae501a16d7165ca3448",
+    "url": "/geo-pins/static/js/runtime~main.0ae501a1.js"
+  },
+  {
+    "revision": "6d8cf1086f5b0bc8a61a",
+    "url": "/geo-pins/static/js/main.6d8cf108.chunk.js"
+  },
+  {
+    "revision": "390737fef92c1f713a79",
+    "url": "/geo-pins/static/js/1.390737fe.chunk.js"
+  },
+  {
+    "revision": "390737fef92c1f713a79",
+    "url": "/geo-pins/static/css/1.35f4ef31.chunk.css"
+  },
+  {
+    "revision": "a3aaf2247d076a39dd1309c5d242dc74",
+    "url": "/geo-pins/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geo-pins/precache-manifest.605a6314053b2badf135840dd69a4f7e.js"
+  "/geo-pins/precache-manifest.e3ff59afd69e3c749cf3de334bfbcf25.js"
 );
 
 workbox.clientsClaim();
