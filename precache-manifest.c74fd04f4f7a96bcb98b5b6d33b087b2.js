0f8dfd21137c5d2366b624c97a2ecd5512e0f42b
ssaavvaa <Toxasava@icloud.com>
2019-08-27 01:13:07 -0400

    Updates

---
+++ precache-manifest.c74fd04f4f7a96bcb98b5b6d33b087b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae501a16d7165ca3448",
+    "url": "/geo-pins/static/js/runtime~main.0ae501a1.js"
+  },
+  {
+    "revision": "86a7984e5aad269b9217",
+    "url": "/geo-pins/static/js/main.86a7984e.chunk.js"
+  },
+  {
+    "revision": "390737fef92c1f713a79",
+    "url": "/geo-pins/static/js/1.390737fe.chunk.js"
+  },
+  {
+    "revision": "390737fef92c1f713a79",
+    "url": "/geo-pins/static/css/1.35f4ef31.chunk.css"
+  },
+  {
+    "revision": "d087b9c28b3f4a6fde0b05676ac56062",
+    "url": "/geo-pins/index.html"
+  }
+];
